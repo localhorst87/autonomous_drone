a7c79cdcfd56524fe6a83e7a39a0f9334acd957c
linux_localhorst87 <m.p.ahmann@gmx.de>
2020-03-22 20:47:27 +0100

    clean-up commit

--- lib_cpp/VideoStream.cpp
+++ lib_cpp/VideoStream.cpp
@@ -33,23 +33,28 @@ Decoder::~Decoder()
 void Decoder::configureDecoder()
 // opens the H264 codec, initializes its parser and allocates the target frame
 {
-  if ( !this->openCodec() ) printf("Error opening the codec! \n");
+  if ( !this->openCodec() )
+    printf("Error opening the codec! \n");
 
   this->parserContext = av_parser_init(this->codecType);
-  if (!this->parserContext) printf("Error initializing parser! \n");
+  if (!this->parserContext)
+    printf("Error initializing parser! \n");
 
   this->frame = av_frame_alloc();
-  if (!this->frame) printf("Error allocating frame! \n");
+  if (!this->frame)
+    printf("Error allocating frame! \n");
 }
 
 bool Decoder::openCodec()
 // returns true/false upon successful/unsucćessful opening of the H264 codec
 {
   AVCodec* codec = avcodec_find_decoder(this->codecType); // returns NULL if codec type not found
-  if (!codec) return false;
+  if (!codec)
+    return false;
 
   this->codecContext = avcodec_alloc_context3(codec); // allocates an AVCodecContext and set its fields to default values
-  if (codec->capabilities & CODEC_CAP_TRUNCATED) this->codecContext->flags |= CODEC_FLAG_TRUNCATED;
+  if (codec->capabilities & CODEC_CAP_TRUNCATED)
+    this->codecContext->flags |= CODEC_FLAG_TRUNCATED;
 
   if (avcodec_open2(codecContext, codec, nullptr) < 0) // returns zero on success, a negative value on error
     return false;
@@ -61,7 +66,7 @@ bool Decoder::parseEncodedData(unsigned char* encodedData, int size)
 // parses the data and returns true if parsing has finished or false if additional encoded data is required.
 {
   int bufferSize = 0; // will be set to size of parsed buffer or 0 if more data is required
-  int nBytes = av_parser_parse2(this->parserContext, this->codecContext, &this->parserBuffer, &bufferSize, encodedData, size, 0, 0, AV_NOPTS_VALUE);
+  av_parser_parse2(this->parserContext, this->codecContext, &this->parserBuffer, &bufferSize, encodedData, size, 0, 0, AV_NOPTS_VALUE);
 
   if (bufferSize > 0)
   {
@@ -86,7 +91,7 @@ bool Decoder::decodeParsedData()
   }
 
   int gotPicture = 0;
-  int bytesDecoded = avcodec_decode_video2(this->codecContext, this->frame, &gotPicture, &this->avPacket);
+  avcodec_decode_video2(this->codecContext, this->frame, &gotPicture, &this->avPacket);
 
   return gotPicture != 0;
 }
